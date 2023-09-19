cdbf0e2f886aca632136213324965f3299607303
Maxime Ortuno <maxime.ortuno@hotmail.fr>
2023-09-19 12:41:11 +0200

    Commente le code

--- my-app/src/assets/KanbanBoard.jsx
+++ my-app/src/assets/KanbanBoard.jsx
@@ -3,22 +3,26 @@ import { DragDropContext } from "react-beautiful-dnd";
 import Column from "./Column";
 
 export default function KanbanBoard() {
+  // État initial des tableaux (boards) avec leurs titres et tâches vides
   const [boards, setBoards] = useState({
-    board1: { title: "TO DO", tasks: [] },
-    board2: { title: "DONE", tasks: [] },
-    board3: { title: "BOB", tasks: [] },
-    // Add more boards as needed
+    board1: { title: "À FAIRE", tasks: [] }, // Tableau "À FAIRE"
+    board2: { title: "TERMINÉ", tasks: [] }, // Tableau "TERMINÉ"
+    board3: { title: "BOB", tasks: [] },      // Tableau "BOB"
+    // Ajoutez davantage de tableaux au besoin
   });
 
+  // Utilisation de useEffect pour récupérer des données initiales depuis une API
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
       .then((json) => {
+        // Transformation des données en tâches initiales avec des ID et du contenu
         const initialTasks = json.map((task) => ({
           id: task.id.toString(),
           content: task.title,
         }));
 
+        // Mise à jour de l'état initial des tâches dans le tableau "À FAIRE"
         setBoards({
           ...boards,
           board1: { ...boards.board1, tasks: initialTasks },
@@ -26,21 +30,28 @@ export default function KanbanBoard() {
       });
   }, []);
 
+  // Fonction pour gérer la fin du glisser-déposer des tâches
   const handleDragEnd = (result) => {
     const { destination, source, draggableId } = result;
-    // Check if the destination exists in boards
+
+    // Vérifie si la destination existe dans les tableaux (boards)
     if (!destination || !boards[destination.droppableId]) {
-      return; // Destination board is missing or undefined, do nothing
+      return; // Le tableau de destination est manquant ou indéfini, ne rien faire
     }
+
     if (source.droppableId === destination.droppableId) return;
 
     const sourceBoard = boards[source.droppableId];
     const destinationBoard = boards[destination.droppableId];
 
+    // Trouver la tâche à déplacer dans le tableau source
     const taskToMove = sourceBoard.tasks.find((task) => task.id === draggableId);
+
+    // Mettre à jour les tâches du tableau source et du tableau de destination
     const updatedSourceTasks = sourceBoard.tasks.filter((task) => task.id !== draggableId);
     const updatedDestinationTasks = [...destinationBoard.tasks, taskToMove];
 
+    // Mettre à jour les tableaux (boards) avec les nouvelles tâches
     setBoards({
       ...boards,
       [source.droppableId]: { ...sourceBoard, tasks: updatedSourceTasks },
@@ -48,21 +59,24 @@ export default function KanbanBoard() {
     });
   };
 
-  // Function to create a new board
+  // Fonction pour créer un nouveau tableau (board)
   const addNewBoard = () => {
     const newBoardId = `board${Object.keys(boards).length + 1}`;
-    const newBoardTitle = `Board ${Object.keys(boards).length + 1}`;
+    const newBoardTitle = `Tableau ${Object.keys(boards).length + 1}`;
+
+    // Ajouter un nouveau tableau avec un titre vide et aucune tâche
     setBoards({
       ...boards,
       [newBoardId]: { title: newBoardTitle, tasks: [] },
     });
   };
 
+  // Rendu de l'application avec les tableaux et le bouton pour ajouter un nouveau tableau
   return (
     <DragDropContext onDragEnd={handleDragEnd}>
-      <h2 style={{ textAlign: "center" }}>KANBAN BOARD</h2>
+      <h2 style={{ textAlign: "center" }}>TABLEAU KANBAN</h2>
       <div>
-        <button onClick={addNewBoard}>Add New Board</button>
+        <button onClick={addNewBoard}>Ajouter un Nouveau Tableau</button>
       </div>
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         {Object.entries(boards).map(([boardId, board]) => (
