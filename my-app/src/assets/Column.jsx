db7f8485c6023e5acb0a5c845c3b5f02eacb8627
Maxime Ortuno <maxime.ortuno@hotmail.fr>
2023-09-19 13:36:45 +0200

    Commentaire

--- my-app/src/assets/Column.jsx
+++ my-app/src/assets/Column.jsx
@@ -3,42 +3,48 @@ import styled from "styled-components";
 import Task from "./Task";
 import { Droppable } from "react-beautiful-dnd";
 
+// Définition d'un composant Container avec des styles CSS
 const Container = styled.div`
   background-color: #f4f5f7;
   border-radius: 2.5px;
-  width: 300px;
+  width: 300px; 
   height: 475px;
   overflow-y: scroll;
-  -ms-overflow-style: none;
-  scrollbar-width: none;
-  border: 1px solid gray;
+  -ms-overflow-style: none; 
+  scrollbar-width: none; 
+  border: 1px solid gray; 
 `;
 
+// Définition d'un composant Title avec des styles CSS
 const Title = styled.h3`
   padding: 8px;
   background-color: pink;
   text-align: center;
 `;
 
+// Définition d'un composant TaskList avec des styles CSS
 const TaskList = styled.div`
   padding: 3px;
-  transistion: background-color 0.2s ease;
-  background-color: #f4f5f7;
+  transition: background-color 0.2s ease; 
+  background-color: #f4f5f7; 
   flex-grow: 1;
   min-height: 100px;
 `;
 
+// Composant Column qui affiche une colonne de tâches
 export default function Column({ title, tasks, id }) {
   return (
     <Container className="column">
+      {/* Composant Title pour le titre de la colonne */}
       <Title
         style={{
-          backgroundColor: "lightblue",
-          position: "stick",
+          backgroundColor: "lightblue", 
+          position: "sticky", 
         }}
       >
         {title}
       </Title>
+      {/* Composant Droppable pour permettre le glisser-déposer de tâches */}
       <Droppable droppableId={id}>
         {(provided, snapshot) => (
           <TaskList
@@ -46,10 +52,11 @@ export default function Column({ title, tasks, id }) {
             {...provided.droppableProps}
             isDraggingOver={snapshot.isDraggingOver}
           >
+            {/* Mapping des tâches pour les afficher dans la colonne */}
             {tasks.map((task, index) => (
               <Task key={index} index={index} task={task} />
             ))}
-            {provided.placeholder}
+            {provided.placeholder} {/* Espace réservé pour les tâches en cours de déplacement */}
           </TaskList>
         )}
       </Droppable>
--- my-app/src/assets/Task.jsx
+++ my-app/src/assets/Task.jsx
@@ -2,50 +2,58 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
+// Définition d'un composant Container avec des styles CSS
 const Container = styled.div`
-  border-radius: 10px;
-  box-shadow: 5px 5px 5px 2px grey;
-  padding: 8px;
-  color: #000;
-  margin-bottom: 8px;
-  min-height: 90px;
-  margin-left: 10px;
-  margin-right: 10px;
-  background-color: ${(props) => bgcolorChange(props)};
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column;
+  border-radius: 10px; // Coins arrondis
+  box-shadow: 5px 5px 5px 2px grey; // Ombre
+  padding: 8px; // Espacement intérieur
+  color: #000; // Couleur du texte
+  margin-bottom: 8px; // Marge en bas
+  min-height: 90px; // Hauteur minimale
+  margin-left: 10px; // Marge à gauche
+  margin-right: 10px; // Marge à droite
+  background-color: ${(props) => bgcolorChange(props)}; // Couleur de fond déterminée dynamiquement
+  cursor: pointer; // Curseur de type pointeur
+  display: flex; // Affichage en tant que boîte flexible
+  justify-content: space-between; // Espace entre les éléments internes
+  flex-direction: column; // Disposition en colonne
 `;
 
+// Composant TextContent pour le contenu textuel
 const TextContent = styled.div``;
 
+// Composant Icons pour les icônes (non implémenté dans le code actuel)
 const Icons = styled.div`
   display: flex;
   justify-content: end;
   padding: 2px;
 `;
+
+// Fonction pour déterminer la couleur de fond en fonction de l'état du glisser-déposer
 function bgcolorChange(props) {
-  return props.isDragging
-    ? "lightgreen"
-    : props.isDraggable
-    ? props.isBacklog
-      ? "#F2D7D5"
-      : "#DCDCDC"
-    : props.isBacklog
-    ? "#F2D7D5"
-    : "#EAF4FC";
+  return props.isDragging // Si l'élément est en cours de glisser-déposer
+    ? "lightgreen" // Couleur de fond en vert clair
+    : props.isDraggable // Si l'élément est déplaçable
+    ? props.isBacklog // Si l'élément appartient à la file d'attente
+      ? "#F2D7D5" // Couleur de fond pour la file d'attente
+      : "#DCDCDC" // Couleur de fond par défaut
+    : props.isBacklog // Si l'élément appartient à la file d'attente (non déplaçable)
+    ? "#F2D7D5" // Couleur de fond pour la file d'attente
+    : "#EAF4FC"; // Couleur de fond par défaut
 }
 
+// Composant Task représentant une tâche dans la colonne
 export default function Task({ task, index }) {
   return (
+    // Utilisation du composant Draggable pour permettre le glisser-déposer
     <Draggable draggableId={`${task.id}`} key={task.id} index={index}>
       {(provided, snapshot) => (
+        // Conteneur de la tâche avec des attributs et des styles
         <Container
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-          ref={provided.innerRef}
-          isDragging={snapshot.isDragging}
+          {...provided.draggableProps} // Propriétés pour le glisser-déposer
+          {...provided.dragHandleProps} // Propriétés pour la poignée de glisser-déposer
+          ref={provided.innerRef} // Référence au conteneur interne
+          isDragging={snapshot.isDragging} // Indicateur si l'élément est en cours de glisser-déposer
         >
           <div style={{ display: "flex", justifyContent: "start", padding: 2 }}>
             <span>
@@ -55,14 +63,12 @@ export default function Task({ task, index }) {
               </small>
             </span>
           </div>
-          <div
-            style={{ display: "flex", justifyContent: "center", padding: 2 }}
-          >
-            <TextContent>{task.title}</TextContent>
+          <div style={{ display: "flex", justifyContent: "center", padding: 2 }}>
+            <TextContent>{task.title}</TextContent> {/* Contenu textuel de la tâche */}
           </div>
-          <Icons>
+          <Icons> {/* Section pour les icônes (non implémentée dans ce code) */}
           </Icons>
-          {provided.placeholder}
+          {provided.placeholder} {/* Espace réservé pour les tâches en cours de déplacement */}
         </Container>
       )}
     </Draggable>
